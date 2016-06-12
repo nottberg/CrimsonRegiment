7bbfc2c75252edcf0cb7d85500f81e4ca7648170
Curtis Nottberg <curtisn@comcast.net>
2016-06-12 12:49:14 -0600

    Experiment with broadcast sockets.

--- led-client-deamon/src/ClientEvents.cpp
+++ led-client-deamon/src/ClientEvents.cpp
@@ -172,13 +172,24 @@ SocketEventSource::~SocketEventSource()
 bool 
 SocketEventSource::setup()
 {
+    int result;
+    static int so_reuseaddr = 1;
+    static int so_broadcast = 1;
+
     socketFD = socket( AF_INET, SOCK_DGRAM, 0 );
 
     memset( &sin, 0, sizeof(sin) );
     sin.sin_family      = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port        = htons( 10000 );
+    sin.sin_addr.s_addr = INADDR_BROADCAST;
+    sin.sin_port        = htons( 10260 );
 	
+    // Allow multiple bind to the broadcast address
+    result = setsockopt( socketFD, SOL_SOCKET, SO_REUSEADDR, &so_reuseaddr, sizeof so_reuseaddr );
+
+    // Set the socket for broadcast traffic
+    result = setsockopt( socketFD, SOL_SOCKET, SO_BROADCAST, &so_broadcast, sizeof so_broadcast );
+
+    // Bind to a local address
     if( bind( socketFD, (struct sockaddr *) &(sin), sizeof(sin) ) ) 
     {
         perror("bind()");
@@ -186,6 +197,87 @@ SocketEventSource::setup()
     }
 
     return false;
+#if 0
+    int z;
+    int x;
+    struct sockaddr_in adr;  /* AF_INET */
+    int len_inet;            /* length */
+    int s;                   /* Socket */
+    char dgram[512];         /* Recv buffer */
+    static int so_reuseaddr = TRUE;
+    static char
+    *bc_addr = "127.255.255.2:9097";
+
+    /*
+     * Use a server address from the command
+     * line, if one has been provided.
+     * Otherwise, this program will default
+     * to using the arbitrary address
+     * 127.0.0.:
+     */
+    if( argc > 1 )
+    /* Broadcast address: */
+       bc_addr = argv[1];
+
+    /*
+     * Create a UDP socket to use:
+     */
+    s = socket( AF_INET, SOCK_DGRAM, 0 );
+    if( s == -1 )
+    {
+        displayError("socket()");
+    }
+
+    /*
+     * Form the broadcast address:
+     */
+    len_inet = sizeof adr;
+
+    z = mkaddr( &adr, &len_inet, bc_addr, "udp" );
+
+    if( z == -1 )
+       displayError("Bad broadcast address");
+
+    /*
+     * Allow multiple listeners on the
+     * broadcast address:
+     */
+    z = setsockopt( s, SOL_SOCKET, SO_REUSEADDR, &so_reuseaddr, sizeof so_reuseaddr );
+
+    if( z == -1 )
+        displayError("setsockopt(SO_REUSEADDR)");
+
+    /*
+     * Bind our socket to the broadcast address:
+     */
+    z = bind( s, (struct sockaddr *)&adr, len_inet );
+
+    if( z == -1 )
+        displayError( "bind(2)" );
+
+    for(;;)
+    {
+        /*
+         * Wait for a broadcast message:
+         */
+        z = recvfrom( s,      /* Socket */
+                      dgram,  /* Receiving buffer */
+                      sizeof dgram,/* Max rcv buf size */
+                      0,      /* Flags: no options */
+                      (struct sockaddr *)&adr, /* Addr */
+                      &x);    /* Addr len, in & out */
+
+        if( z < 0 )
+            displayError( "recvfrom(2)" ); /* else err */
+
+        fwrite( dgram, z, 1, stdout );
+        putchar( '\n' );
+
+        fflush( stdout );
+    }
+
+    return 0;
+#endif
 }
 
 void 
@@ -199,6 +291,16 @@ SocketEventSource::registerEvent()
 void 
 SocketEventSource::eventAction( const int arg1, short int which )
 {
+    int                bytesRead;
+    uint8_t            pktBuf[2048];
+    struct sockaddr_in addr; 
+    unsigned int       addrLen;
+
     std::cout << "SocketEventSource::eventAction" << std::endl;
+
+    bytesRead = recvfrom( socketFD, pktBuf, sizeof( pktBuf ), 0, (struct sockaddr *) &addr, &addrLen );
+
+    std::cout << "SocketEventSource::eventAction - byteRead: " << bytesRead << std::endl;
+
 }
 
