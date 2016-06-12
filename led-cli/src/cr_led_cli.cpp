797c5398f26901c32b9df84675ad6d0922c547a1
Curtis Nottberg <curtisn@comcast.net>
2016-06-12 15:50:34 -0600

    Start off a simple cli utility.

---
+++ led-cli/src/cr_led_cli.cpp
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+#include <iostream>
+
+int
+main( int argc, char **argv )
+{
+    struct sockaddr_in s;
+    int bcast_sock;
+    int broadcastEnable=1;
+    int result;
+    uint8_t msgBuf[1024];
+
+    bcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    result = setsockopt( bcast_sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable) );
+
+    memset( &s, '\0', sizeof( struct sockaddr_in ) );
+    s.sin_family      = AF_INET;
+    s.sin_port        = (in_port_t)htons( 10260 );
+    s.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
+    std::cout << "Send" << std::endl;
+
+    if( sendto( bcast_sock, msgBuf, 60, 0, (struct sockaddr *)&s, sizeof(struct sockaddr_in) ) < 0 )
+    {
+        perror("sendto");
+        return 1;
+    }
+
+    return 0;
+}
+
