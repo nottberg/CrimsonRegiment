6e7cd1bb9181583d7a7c4cfc197a4ac16fada909
Curtis Nottberg <curtisn@comcast.net>
2016-06-13 07:56:21 -0600

    Add some program options and other refactoring.

--- led-cli/src/cr_led_cli.cpp
+++ led-cli/src/cr_led_cli.cpp
@@ -5,302 +5,103 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 
 #include <iostream>
 
-typedef enum crledCommandOperation
-{
-    CRLED_CMDOP_NOTSET   = 0x0,
-    CRLED_CMDOP_PING     = 0x1,
-    CRLED_CMDOP_SCHEDULE = 0x2,
-    CRLED_CMDOP_CLEAR    = 0x3
-}CRLED_CMDOP_T;
-        
-typedef struct crledCommandPacket
-{
-    uint32_t opcode;
-    uint32_t srcid;
-    uint32_t reqnum;
-    uint32_t tsec;
-    uint32_t tusec;
-    uint32_t param1;
-    uint32_t param2;
-    uint32_t param3;
-    uint32_t param4;
-    uint32_t addlength;
-}CRLED_CMDPKT_T;
+#include <boost/program_options.hpp>
 
-class CRLEDCommandPacket
-{
-    private:
-        uint8_t  msgBuf[ 1500 ];
-        uint32_t msgLength;
+#include "CRLEDPacket.hpp"
 
-    public:
-        CRLEDCommandPacket();
-       ~CRLEDCommandPacket();
-
-        void clear();
-
-        uint32_t getOpCode();
-        uint32_t getSrcID();
-        uint32_t getReqNum();
-        uint32_t getTSSec();
-        uint32_t getTSUSec();
-        uint32_t getParam1();
-        uint32_t getParam2();
-        uint32_t getParam3();
-        uint32_t getParam4();
-        uint32_t getAddLength();
-
-        void setOpCode( uint32_t value );
-        void setSrcID( uint32_t value );
-        void setReqNum( uint32_t value );
-        void setTSSec( uint32_t value );
-        void setTSUSec( uint32_t value );
-        void setParam1( uint32_t value );
-        void setParam2( uint32_t value );
-        void setParam3( uint32_t value );
-        void setParam4( uint32_t value );
-        void setAddLength( uint32_t value );
-
-        uint32_t getMessageLength();
-        void     setMessageLength( uint32_t value );
-
-        uint32_t getMaxMessageLength();
-        uint8_t* getMessageBuffer();
-};
-
-CRLEDCommandPacket::CRLEDCommandPacket()
-{
-    clear();
-
-    msgLength = sizeof( CRLED_CMDPKT_T );
-}
-
-CRLEDCommandPacket::~CRLEDCommandPacket()
-{
-
-}
-
-void 
-CRLEDCommandPacket::clear()
-{
-    bzero( msgBuf, sizeof( msgBuf ) );
-}
-
-uint32_t 
-CRLEDCommandPacket::getMessageLength()
-{
-    return msgLength;
-}
-
-void  
-CRLEDCommandPacket::setMessageLength( uint32_t value )
-{
-    msgLength = value;
-}
-
-uint8_t* 
-CRLEDCommandPacket::getMessageBuffer()
-{
-    return msgBuf;
-}
-
-uint32_t 
-CRLEDCommandPacket::getMaxMessageLength()
-{
-    return sizeof( msgBuf );
-}
-
-uint32_t 
-CRLEDCommandPacket::getOpCode()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->opcode );
-}
-
-uint32_t 
-CRLEDCommandPacket::getSrcID()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->srcid );
-}
-
-uint32_t 
-CRLEDCommandPacket::getReqNum()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->reqnum );
-}
-
-uint32_t 
-CRLEDCommandPacket::getTSSec()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->tsec );
-}
-
-uint32_t 
-CRLEDCommandPacket::getTSUSec()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->tusec );
-}
-
-uint32_t 
-CRLEDCommandPacket::getParam1()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->param1 );
-}
-
-uint32_t 
-CRLEDCommandPacket::getParam2()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->param2 );
-}
-
-uint32_t 
-CRLEDCommandPacket::getParam3()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->param3 );
-}
-
-uint32_t 
-CRLEDCommandPacket::getParam4()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->param4 );
-}
-
-uint32_t 
-CRLEDCommandPacket::getAddLength()
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    return ntohl( header->addlength );
-}
-
-void 
-CRLEDCommandPacket::setOpCode( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->opcode = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setSrcID( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->srcid = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setReqNum( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->reqnum = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setTSSec( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->tsec = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setTSUSec( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->tusec = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setParam1( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->param1 = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setParam2( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->param2 = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setParam3( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->param3 = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setParam4( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->param4 = htonl( value );
-}
-
-void 
-CRLEDCommandPacket::setAddLength( uint32_t value )
-{
-    CRLED_CMDPKT_T *header = (CRLED_CMDPKT_T *)msgBuf;
-
-    header->addlength = htonl( value );
-}
+namespace po = boost::program_options;
 
 int
 main( int argc, char **argv )
 {
-    struct sockaddr_in s;
-    int bcast_sock;
-    int broadcastEnable=1;
-    int result;
-//    uint8_t msgBuf[1024];
-    CRLEDCommandPacket cmdPkt;
+    int schSeqNum;
+    struct timeval curTime;
 
-    cmdPkt.setOpCode( CRLED_CMDOP_SCHEDULE );
+    // Declare the supported options.
+    po::options_description desc("Crimson Regiment LED command line control");
+    desc.add_options()
+        ("help", "produce help message")
+        ("schedule,s", po::value<int>(&schSeqNum), "Schedule a sequence.")
+        ("clear,c", "Clear currently running sequences.")
+    ;
 
-    bcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
-    
-//    result = setsockopt( bcast_sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable) );
+    po::variables_map vm;
+    po::store( po::parse_command_line( argc, argv, desc ), vm );
+    po::notify( vm );    
 
-    memset( &s, '\0', sizeof( struct sockaddr_in ) );
-    s.sin_family      = AF_INET;
-    s.sin_port        = (in_port_t)htons( 10260 );
-    inet_aton( "192.168.2.7", &s.sin_addr );
-
-    std::cout << "Send" << std::endl;
-
-    
-    if( sendto( bcast_sock, cmdPkt.getMessageBuffer(), cmdPkt.getMessageLength(), 0, (struct sockaddr *)&s, sizeof(struct sockaddr_in) ) < 0 )
+    if (vm.count("help")) 
     {
-        perror("sendto");
+       std::cout << desc << "\n";
+       return 1;
+    }
+
+    // Get the current time.
+    if( gettimeofday( &curTime, NULL ) ) 
+    {
+        perror("gettimeofday()");
         return 1;
+    }
+
+    if( vm.count( "schedule" ) ) 
+    {
+        struct sockaddr_in s;
+        int bcast_sock;
+        int result;
+        CRLEDCommandPacket cmdPkt;
+ 
+        std::cout << "Sending start sequence: " << schSeqNum << std::endl;
+
+        cmdPkt.setOpCode( CRLED_CMDOP_SCHEDULE );
+        cmdPkt.setParam1( schSeqNum );
+        cmdPkt.setTSSec( curTime.tv_sec );
+        cmdPkt.setTSUSec( curTime.tv_usec );
+
+        bcast_sock = socket( AF_INET, SOCK_DGRAM, 0 );
+    
+        memset( &s, '\0', sizeof( struct sockaddr_in ) );
+        s.sin_family      = AF_INET;
+        s.sin_port        = (in_port_t)htons( 10260 );
+        inet_aton( "192.168.2.7", &s.sin_addr );
+
+        std::cout << "Send" << std::endl;
+
+        if( sendto( bcast_sock, cmdPkt.getMessageBuffer(), cmdPkt.getMessageLength(), 0, (struct sockaddr *)&s, sizeof(struct sockaddr_in) ) < 0 )
+        {
+            perror("sendto");
+            return 1;
+        }
+    } 
+    else if( vm.count( "clear" ) )
+    {
+        struct sockaddr_in s;
+        int bcast_sock;
+        int result;
+        CRLEDCommandPacket cmdPkt;
+
+        std::cout << "Sending clear sequence" << std::endl;
+
+        cmdPkt.setOpCode( CRLED_CMDOP_CLEAR );
+        cmdPkt.setTSSec( curTime.tv_sec );
+        cmdPkt.setTSUSec( curTime.tv_usec );
+
+        bcast_sock = socket( AF_INET, SOCK_DGRAM, 0 );
+    
+        memset( &s, '\0', sizeof( struct sockaddr_in ) );
+        s.sin_family      = AF_INET;
+        s.sin_port        = (in_port_t)htons( 10260 );
+        inet_aton( "192.168.2.7", &s.sin_addr );
+
+        std::cout << "Send" << std::endl;
+
+        if( sendto( bcast_sock, cmdPkt.getMessageBuffer(), cmdPkt.getMessageLength(), 0, (struct sockaddr *)&s, sizeof(struct sockaddr_in) ) < 0 )
+        {
+            perror("sendto");
+            return 1;
+        }
     }
 
     return 0;
