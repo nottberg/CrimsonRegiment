9eaa04ea61b37df6559a3d7359897c25d6eec673
Curtis Nottberg <curtisn@comcast.net>
2016-10-01 09:41:21 -0600

    Refactor the script to add parameters, etc.

--- tools/updateClient.py
+++ tools/updateClient.py
@@ -1,40 +1,77 @@
+#!/usr/bin/python
+import sys
 import pexpect
 import pxssh
+import argparse
 
-p = pxssh.pxssh()
-p.login( "192.168.2.8", "pi", "raspberry" )
-p.sendline('netstat -n -t')
-PROMPT = p.PROMPT
+def main():
+    parser = argparse.ArgumentParser(description='Update node client code')
+    parser.add_argument('-a', '--address', help='IPv4 address of node to update.')
+    parser.add_argument('-p', '--package', help='The package file to use for the update.')
 
-p.expect(PROMPT)
-print "Netstat Done"
-print p.before
+    args = parser.parse_args()
 
-p.sendline('scp pi@192.168.2.3:/home/pi/Projects/CrimsonRegiment/led-client-deamon/build/cr-led-clientd-1.0.3-Linux.deb .')
-p.expect('password')
-p.sendline('raspberry')
-p.expect( PROMPT )
+    if( args.address == None ):
+        print "ERROR: A node address argument is required. (--address or -a)."
+        sys.exit(1)
 
-print "cr-led-clientd-1.0.3-Linux.deb fetch done"
-print p.before
+    if( args.package == None ):
+        print "ERROR: A node package argument is required. (--package or -p)."
+        sys.exit(1)
 
-p.sendline('sudo dpkg -i cr-led-clientd-1.0.3-Linux.deb')
-p.expect( PROMPT, timeout=120 )
+    print "=== Start update of " + args.address + " with " + args.package
 
-print "cr led client install done"
-print p.before
+    login = "pi"
+    pword = "raspberry"
+    getPkgCmd = 'scp ' + login + '@192.168.2.3:/home/pi/Projects/CrimsonRegiment/led-client-deamon/build/' + args.package + ' .'
+    installPkgCmd = 'sudo dpkg -i ' + args.package
 
-p.sendline('sudo service cr-led-clientd restart')
-p.expect( PROMPT )
+    p = pxssh.pxssh()
+    p.login( args.address, login, pword )
+    p.sendline('netstat -n -t')
+    PROMPT = p.PROMPT
 
-print "client restart done"
-print p.before
+    p.expect(PROMPT)
+    print "Netstat Done"
+    print p.before
 
-p.sendline('sudo service cr-led-clientd status')
-p.expect( PROMPT )
+    p.sendline( getPkgCmd )
+    i=p.expect(['yes/no','password'])
+    if(i==0):
+        p.sendline('yes')
+        p.expect('password')
 
-print "client status done"
-print p.before
+    p.sendline(pword)
+    p.expect( PROMPT )
 
-p.sendline('exit');
-print 'Doner'
+    print "package fetch complete"
+    print p.before
+
+    p.sendline( installPkgCmd )
+    p.expect( PROMPT, timeout=120 )
+
+    print "package install done"
+    print p.before
+
+    p.sendline('sudo service cr-led-clientd restart')
+    p.expect( PROMPT )
+
+    print "client restart done"
+    print p.before
+
+    p.sendline('sudo service cr-led-clientd status')
+    p.expect( PROMPT )
+
+    print "client status done"
+    print p.before
+
+    p.sendline('exit');
+    print 'Doner'
+
+    print "=== Complete ==="
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
--- tools/updateSequence.py
+++ tools/updateSequence.py
@@ -1,34 +1,61 @@
+#!/usr/bin/python
+import sys
 import pexpect
 import pxssh
+import argparse
 
-p = pxssh.pxssh()
-p.login( "192.168.2.8", "pi", "raspberry" )
-p.sendline('netstat -n -t')
-PROMPT = p.PROMPT
+def main():
+    parser = argparse.ArgumentParser(description='Update node client code')
+    parser.add_argument('-a', '--address', help='IPv4 address of node to update.')
 
-p.expect(PROMPT)
-print "Netstat Done"
-print p.before
+    args = parser.parse_args()
 
-p.sendline('scp pi@192.168.2.3:/home/pi/Projects/CrimsonRegiment/setup-files/client/etc/crled/sequence.xml /etc/crled/sequence.xml')
-p.expect('password')
-p.sendline('raspberry')
-p.expect( PROMPT )
+    if( args.address == None ):
+        print "ERROR: A node address argument is required. (--address or -a)."
+        sys.exit(1)
 
-print "new sequence file copy done"
-print p.before
+    print "=== Updating sequence for " + args.address + " ==="
 
-p.sendline('sudo service cr-led-clientd restart')
-p.expect( PROMPT )
+    login = "pi"
+    pword = "raspberry"
+    getSequenceCmd = 'scp ' + login + '@192.168.2.3:/home/pi/Projects/CrimsonRegiment/setup-files/client/etc/crled/sequence.xml /etc/crled/sequence.xml'
 
-print "client restart done"
-print p.before
+    p = pxssh.pxssh()
+    p.login( args.address, login, pword )
+    p.sendline('netstat -n -t')
+    PROMPT = p.PROMPT
 
-p.sendline('sudo service cr-led-clientd status')
-p.expect( PROMPT )
+    p.expect(PROMPT)
+    print "Netstat Done"
+    print p.before
 
-print "client status done"
-print p.before
+    p.sendline(getSequenceCmd)
+    i=p.expect(['yes/no','password'])
+    if(i==0):
+        p.sendline('yes')
+        p.expect('password')
 
-p.sendline('exit');
-print 'Doner'
+    p.sendline(pword)
+    p.expect( PROMPT )
+
+    print "new sequence file copy done"
+    print p.before
+
+    p.sendline('sudo service cr-led-clientd restart')
+    p.expect( PROMPT )
+
+    print "client restart done"
+    print p.before
+
+    p.sendline('sudo service cr-led-clientd status')
+    p.expect( PROMPT )
+
+    print "client status done"
+    print p.before
+
+    p.sendline('exit');
+    print 'Doner'
+
+if __name__ == "__main__":
+    main()
+
